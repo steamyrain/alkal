ab82de840125343f955212351b711c4349b45eed
rain <rain@pop-os.localdomain>
2020-11-24 10:55:16 +0700

    fixed: add rules in middleware

--- application/middleware/AuthCheckMiddleware.php
+++ application/middleware/AuthCheckMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AuthCheckMiddleware implements Luthier\MiddlewareInterface
@@ -15,6 +14,33 @@ class AuthCheckMiddleware implements Luthier\MiddlewareInterface
         if (empty(Auth::user())){
             return redirect(route('homepage'));
         }
+        else if ((Auth::user()->getRoles() !== 'admin') && $this->_rules_admin()){
+            return redirect(route('dashboard-user'));
+        }
+        else if ((Auth::user()->getRoles() !== 'user') && $this->_rules_user()){
+            return redirect(route('dashboard-admin'));
+        }
         return;
+    }
+
+    private function _rules_admin(){
+        return (
+                    (ci()->route->getName() == 'dashboard-admin') ||
+                    (ci()->route->getName() == 'kinerja-admin') ||
+                    (ci()->route->getName() == 'kinerja-admin-form') ||
+                    (ci()->route->getName() == 'user-table-admin') ||
+                    (ci()->route->getName() == 'adduser-admin-form') ||
+                    (ci()->route->getName() == 'adduser-admin-input') ||
+                    (ci()->route->getName() == 'kinerja-admin-input')
+        );
+    }
+
+    private function _rules_user(){
+        return (
+                    (ci()->route->getName() == 'dashboard-user') ||
+                    (ci()->route->getName() == 'kinerja-user') ||
+                    (ci()->route->getName() == 'kinerja-user-form') ||
+                    (ci()->route->getName() == 'kinerja-user-input')
+        );
     }
 }
